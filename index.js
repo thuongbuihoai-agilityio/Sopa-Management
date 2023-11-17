40ac5f3eb2e01fb34bac00c88cecda1961431cee
Thuong Bui Hoai <thuong.buihoai@asnet.com.vn>
2023-11-17 16:11:02 +0700

    deploy json-server

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // server.js
-import jsonServer from "json-server";
-
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
